e36ceef4bc8a96b14951d43383cf25b399bdc400
Marcos Vinícius <13080marcos.tupa@gmail.com>
2021-10-12 19:01:30 -0300

    Desafio concluído

--- 03_fix_start.py
+++ 03_fix_start.py
@@ -14,11 +14,9 @@ onde todas as instancias de stra foram substituidas por strb.
 """
 
 def fix_start(s):
-    # +++ SUA SOLUÇÃO +++
-    return
 
+    return s[0] + s[1:].replace(s[0], '*')
 
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
 def test(f, in_, expected):
     """
--- 04_mix_up.py
+++ 04_mix_up.py
@@ -12,12 +12,10 @@ Exemplo:
 Assuma que a e b tem tamanho 2 ou maior.
 """
 
+
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    return " ".join([b[:2]+a[2:], a[:2]+b[2:]])
 
-
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
 def test(f, in_, expected):
     """
